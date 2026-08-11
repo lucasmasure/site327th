a844c351acf4dd52147ad1e7bc00529d696d4110
TWG <111008616+lucasmasure@users.noreply.github.com>
2026-08-11 22:16:25 +0200

    Update img-overrides.js

--- img-overrides.js
+++ img-overrides.js
@@ -89,7 +89,7 @@ const IMG_OVERRIDES = {
   "rc1.png": "https://imgur.com/0Es37x9.png",
   "rc3.jpg": "https://imgur.com/R6BxxDJ.png",
   // Mando pour les nuls
-  "mando.png": "https://imgur.com/32wwHUE.png",
+  "mando.PNG": "https://imgur.com/32wwHUE.png",
   // radio
   "radio1.png": "https://imgur.com/MQXKANY.png",
   "radio2.png": "https://imgur.com/ZZLJc7x.png",
